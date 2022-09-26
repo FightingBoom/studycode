6eb9d49fb1a1b528a56d57aaea4d6b4bb7fcf87a
FightingBoom <1446227671@qq.com>
2022-09-26 23:27:20 +0800

    add code_4.20

---
+++ c++PrimerPlus/chapter_4/code_4.20/ptrstr.cpp
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{    
+    char animal[20] = "bear";
+    const char * bird = "wren";
+    char * ps;
+    
+    cout << animal << " and ";
+    cout << bird << "\n";
+    // cout << ps << "\n";
+    
+    cout << "Enter a kind of animal: ";
+    cin >> animal;
+    
+    ps = animal;
+
+    cout << ps << "!\n";
+    cout << "Before using strcpy():\n";
+    cout << animal << " at " << (int *) animal << endl;
+    cout << ps << " at " << (int *) ps << endl;
+
+    ps = new char[strlen(animal) + 1];
+    strcpy(ps, animal);
+    cout << "After using strcpy():\n";
+    cout << animal << " at " << (int *) animal << endl;
+    cout << ps << " at " << (int *) ps << endl;
+    delete [] ps;
+
+    return 0;
+}
+
+
+/* 运行结果如下
+bear and wren
+Enter a kind of animal: zhaochen!
+Before using strcpy():
+zhaochen at 0x7ffce29236f0
+zhaochen at 0x7ffce29236f0
+After using strcpy():
+zhaochen at 0x7ffce29236f0
+zhaochen at 0x563075f29e90
+*/
