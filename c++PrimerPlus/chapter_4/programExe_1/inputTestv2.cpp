c036a251cca9d82c4abc34f0264198a03e0a0fbc
FightingBoom <1446227671@qq.com>
2022-10-04 23:42:07 +0800

    1、第一次测试未看清要求，分数不能自动加一； 2、第二次测试，修改了代码逻辑，直接使用变量，更简洁；

---
+++ c++PrimerPlus/chapter_4/programExe_1/inputTestv2.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+    char cFirstName[20];    // first name，根据需要增大数组容量
+    char cLastName[20];     // last name，根据需要增大数组容量
+    char cGrade;            // grade
+    int iAge;               // age
+
+    cout << "What is your first name? ";
+    cin.getline(cFirstName, 20);
+    cin.clear();
+    cin.sync();
+
+    cout << "What is your last name? ";
+    cin.getline(cLastName, 20);
+    cin.clear();
+    cin.sync();
+
+    cout << "What letter grade do you deserve? ";
+    cin.get(cGrade);
+    cin.clear();
+    cin.sync();
+
+    cout << "What is your age? ";
+    cin >> iAge;
+    cin.clear();
+    cin.sync();
+
+    cout << "Name: " << cLastName << ", " << cFirstName << endl;
+    cout << "Grade: " << (char)(cGrade + 1) << endl;
+    cout << "Age: " << iAge << endl;
+
+    return 0;
+}
+
+
+/*
+What is your first name? Betty Sue
+What is your last name? Yewe
+What letter grade do you deserve? B
+What is your age? 22
+Name: Yewe, Betty Sue
+Grade: C
+Age: 22
+*/
+
+/* 思考点
+1、cin 输入需要使用 getline ，否则输入内容有空格会被截断；
+2、cin 每次输入后，需要 clear 、sync 清空缓存区，否则后续的输入不会被执行；
+3、数字最好使用整型，char 类型只会打印出来一个字符；
+*/
