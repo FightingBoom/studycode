24aa2bb6d92068b41252d3eddac8b8f4ceae95eb
FightingBoom <1446227671@qq.com>
2022-10-05 17:35:26 +0800

    add code_4.07

---
+++ c++PrimerPlus/chapter_4/code_4.07/strtype1.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cstring>
+
+int main()
+{
+    using namespace std;
+
+    cout << "What year was your house built?\n";
+
+    char charr1[20];
+    char charr2[20] = "jaguar";
+    string str1;
+    string str2 = "panther";
+
+    cout << "Enter a kind of feline: ";
+    cin >> charr1;
+
+    cout << "Enter another kind of feline: ";
+    cin >> str1;
+
+    cout << "Here are some felines:\n";
+    cout << charr1 << " " << charr2 << " " << str1 << " " << str2 << endl;
+    cout << "The third letter in " << charr2 << " is " << charr2[2] << endl;
+    cout << "The third letter in " << str2 << " is " << str2[2] << endl;
+    
+
+    return 0;
+}
+
+
+/*
+What year was your house built?
+Enter a kind of feline: ocelot
+Enter another kind of feline: tiger
+Here are some felines:
+ocelot jaguar tiger panther
+The third letter in jaguar is g
+The third letter in panther is n
+*/
