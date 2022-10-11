df51c9078cd1685624a9f68b0ee14e283d532d75
FightingBoom <1446227671@qq.com>
2022-10-11 23:19:09 +0800

    add code_5.15

---
+++ c++PrimerPlus/chapter_05/code_5.15/dowhile.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cout << "Enter numberes in the range 1-10 to find my favorite number\n";
+
+    do
+    {
+        cin >> n;
+    } while (n != 7);
+
+    cout << "Yes, 7 is my favorite.\n";
+
+    return 0;
+}
+
+
+/*
+Enter numberes in the range 1-10 to find my favorite number
+1
+2
+3
+4
+5
+6
+8
+9
+5
+3
+7
+Yes, 7 is my favorite. 
+*/
+
