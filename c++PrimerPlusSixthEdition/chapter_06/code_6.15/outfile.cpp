1b65626ba0368adc59e292b409b6a07b512d5fce
FightingBoom <1446227671@qq.com>
2022-11-07 22:26:23 +0800

    add code_6.15. 学习简单的写入文件到文本。

---
+++ c++PrimerPlusSixthEdition/chapter_06/code_6.15/outfile.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    char automobile[50];
+    int year;
+    double a_price;
+    double d_price;
+
+    ofstream outFile;
+    outFile.open("carinfo.txt");    // 打开 carinfo.txt ，使用结束必须关闭
+
+    cout << "Enter the make and model of automobile: ";
+    cin.getline(automobile, 50);
+    
+    cout << "Enter the model year: ";
+    cin >> year;
+
+    cout << "Enter the original asking price: ";
+    cin >> a_price;
+    d_price = 0.913 * a_price;
+
+    cout << fixed;
+    cout.precision(2);
+    cout.setf(ios_base::showpoint);
+    cout << "Make and model: " << automobile << endl;
+    cout << "Year: " << year << endl;
+    cout << "Was asking $" << a_price << endl;
+    cout << "Now asking $" << d_price << endl;
+
+    outFile << fixed;
+    outFile.precision(2);
+    outFile.setf(ios_base::showpoint);
+    outFile << "Make and model: " << automobile << endl;
+    outFile << "Year: " << year << endl;
+    outFile << "Was asking $" << a_price << endl;
+    outFile << "Now asking $" << d_price << endl;
+
+    outFile.close();
+    return 0;
+}
+
+/*
+Enter the make and model of automobile: Flitz Perky
+Enter the model year: 2009
+Enter the original asking price: 13500
+Make and model: Flitz Perky
+Year: 2009
+Was asking $13500.00
+Now asking $12325.50
+*/
+
