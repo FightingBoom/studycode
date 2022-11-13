3db5860cb3c94463fb67678279d7e5b2954352f5
FightingBoom <1446227671@qq.com>
2022-11-13 17:38:28 +0800

    add code_7.01, 学习函数调用

---
+++ c++PrimerPlusSixthEdition/chapter_07/code_7.01/calling.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+void simple();
+
+int main()
+{
+    cout << "main() will call the simple() function:\n";
+    simple();
+    cout << "main() is finished with the simple() function.\n";
+
+    return 0;
+}
+
+void simple()
+{
+    cout << "I'm but a simple function.\n";
+}
+
+/*
+main() will call the simple() function:
+I'm but a simple function.
+main() is finished with the simple() function.
+*/
