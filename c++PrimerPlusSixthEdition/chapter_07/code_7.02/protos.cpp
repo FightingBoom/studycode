5d8bb84ff180291b61b803710ffa3b4ecb84c4bf
FightingBoom <1446227671@qq.com>
2022-11-16 22:53:46 +0800

    add code_7.02, 学习带返回值的函数调用；

---
+++ c++PrimerPlusSixthEdition/chapter_07/code_7.02/protos.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+// 函数原型声明
+void cheers(int n);
+double cube(double x);
+
+
+int main()
+{
+    cheers(5);
+    cout << "Give me a number: ";
+    double side;
+    cin >> side;
+    double volume = cube(side);
+    cout << "A " << side << "-foot cube has a volume of ";
+    cout << volume << " cubic feet.\n";
+    cheers(cube(2));
+
+    return 0;
+}
+
+
+void cheers(int n)
+{
+    for (int i = 0; i < n; i++)
+        cout << "Cheers! ";
+    cout << endl;
+}
+
+
+double cube(double x)
+{
+    return x * x * x;
+}
+
+
+/*
+Cheers! Cheers! Cheers! Cheers! Cheers! 
+Give me a number: 1.5
+A 1.5-foot cube has a volume of 3.375 cubic feet.
+Cheers! Cheers! Cheers! Cheers! Cheers! Cheers! Cheers! Cheers!
+*/
