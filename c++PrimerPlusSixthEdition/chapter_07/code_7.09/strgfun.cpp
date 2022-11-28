e132140a20d31bbc56639c23684addade49d5e82
FightingBoom <1446227671@qq.com>
2022-11-28 22:28:06 +0800

    add code_7.09,学习字符串传递函数

---
+++ c++PrimerPlusSixthEdition/chapter_07/code_7.09/strgfun.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+unsigned int c_in_str(const char * str, char ch);
+
+int main()
+{
+    char mmm[15] = "minimum";
+    char *wail = "ululate";
+
+    unsigned int ms = c_in_str(mmm, 'm');
+    unsigned int us = c_in_str(wail, 'u');
+    cout << ms << " m characters in " << mmm << endl;
+    cout << us << " u characters in " << wail << endl;
+
+    return 0;
+}
+
+unsigned int c_in_str(const char * str, char ch)
+{
+    unsigned int count = 0;
+    while (*str)
+    {
+        if (*str == ch)
+            count++;
+        str++;
+    }
+    return count;
+}
+
+/*
+strgfun.cpp: In function 'int main()':
+strgfun.cpp:10:18: warning: ISO C++ forbids converting a string constant to 'char*' [-Wwrite-strings]
+     char *wail = "ululate";
+                  ^~~~~~~~~
+3 m characters in minimum
+2 u characters in ululate
+
+
+
+*/
+
