146ad23a0f9601dbf8638406c72970b3d9255f79
FightingBoom <1446227671@qq.com>
2022-12-11 17:29:46 +0800

    add code_8.07，学习类对象作为引用

---
+++ c++PrimerPlusSixthEdition/chapter_08/code_8.07/strquote.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string version1(const string & s1, const string & s2);
+const string & version2(string & s1, const string & s2);
+const string & version3(string & s1, const string & s2); // 不好的设计
+
+int main()
+{
+    string input;
+    string copy;
+    string result;
+
+    cout << "Enter a string: ";
+    getline(cin, input);
+    copy = input;
+    cout << "Your string as entered: " << input << endl;
+    result = version1(input, "***");
+    cout << "Your string enhanced: " << result << endl;
+    cout << "Your original string: " << input << endl;
+
+    result = version2(input, "###");
+    cout << "Your string enhanced: " << result << endl;
+    cout << "Your original string: " << input << endl;
+
+    cout << "Resetting original string.\n";
+    input = copy;
+    result = version3(input, "@@@");
+    cout << "Your string enhanced: " << result << endl;
+    cout << "Your original string: " << input << endl;
+
+    return 0;
+}
+
+string version1(const string & s1, const string & s2)
+{
+    string temp;
+    temp = s2 + s1 + s2;
+    return temp;
+}
+
+const string & version2(string & s1, const string & s2)
+{
+    s1 = s2 + s1 + s2;
+    return s1;
+}
+
+const string & version3(string & s1, const string & s2)
+{
+    string temp;
+    temp = s2 + s1 + s2;
+    return temp; // 引用返回临时变量，有风险
+}
+
+
+/*
+strquote.cpp: In function 'const string& version3(std::__cxx11::string&, const string&)':
+strquote.cpp:52:12: warning: reference to local variable 'temp' returned [-Wreturn-local-addr]
+     string temp;
+            ^~~~
+Enter a string: It's not my fault.
+Your string as entered: It's not my fault.
+Your string enhanced: ***It's not my fault.***
+Your original string: It's not my fault.
+Your string enhanced: ###It's not my fault.###
+Your original string: ###It's not my fault.###
+Resetting original string.
+*/
