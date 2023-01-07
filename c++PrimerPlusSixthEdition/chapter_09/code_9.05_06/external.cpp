c9c7e7710140a076352b2156573c5a0443ef9972
FightingBoom <1446227671@qq.com>
2023-01-07 10:20:44 +0800

    增加运算结果

--- c++PrimerPlusSixthEdition/chapter_09/code_9.05_06/external.cpp
+++ c++PrimerPlusSixthEdition/chapter_09/code_9.05_06/external.cpp
@@ -17,3 +17,15 @@ int main()
 
     return 0;
 }
+
+/*
+PS E:\91-GitRepository\studycode\c++PrimerPlusSixthEdition\chapter_09\code_9.05_06> g++ .\external.cpp .\support.cpp
+PS E:\91-GitRepository\studycode\c++PrimerPlusSixthEdition\chapter_09\code_9.05_06>
+PS E:\91-GitRepository\studycode\c++PrimerPlusSixthEdition\chapter_09\code_9.05_06> .\a.exe
+Global warming is 0.3 degrees.
+Updating global warming to 0.4 degrees.
+Global warming is 0.4 degrees.
+Local warming = 0.8 degrees.
+But global warming = 0.4 degrees.
+Global warming is 0.4 degrees.
+*/
