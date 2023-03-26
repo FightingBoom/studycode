b05ccd432e416a2e2b4821717a98e22aadc6392f
FightingBoom <1446227671@qq.com>
2023-03-26 17:39:07 +0800

    重载运算符-，交换位置测试

--- c++PrimerPlusSixthEdition/chapter_11/code_11.07_08_09/usetime2.cpp
+++ c++PrimerPlusSixthEdition/chapter_11/code_11.07_08_09/usetime2.cpp
@@ -25,8 +25,10 @@ int main()
     total.Show();
     cout << endl;
 
-    diff = weeding - waxing;  // 使用重载运算符-
-    cout << "weeding time - waxing time = ";
+    // diff = weeding - waxing;  // 使用重载运算符-
+    diff = waxing - weeding;  // 使用重载运算符-，交换位置测试
+    // cout << "weeding time - waxing time = ";
+    cout << "waxing time - weeding time = ";
     diff.Show();
     cout << endl;
 
@@ -43,5 +45,13 @@ weeding time = 4 hours, 35 minutes
 waxing time = 2 hours, 47 minutes
 total work time = 7 hours, 22 minutes
 weeding time - waxing time = 1 hours, 48 minutes
+adjusted work time = 11 hours, 3 minutes
+*/
+
+/* 重载运算符-，交换位置
+weeding time = 4 hours, 35 minutes
+waxing time = 2 hours, 47 minutes
+total work time = 7 hours, 22 minutes
+waxing time - weeding time = -1 hours, -48 minutes
 adjusted work time = 11 hours, 3 minutes
 */
