911143eeed0c868e7d0f0299e79ef72248d84595
FightingBoom <1446227671@qq.com>
2023-04-02 19:42:20 +0800

    增加将类类型转换为int/double类型的测试代码

---
+++ c++PrimerPlusSixthEdition/chapter_11/code_11.19_20_21/stone1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "stonewt1.h"
+
+int main()
+{
+    using std::cout;
+
+    Stonewt poppins(9, 2.8);
+    double p_wt = poppins;
+
+    cout << "Convert to double => ";
+    cout << "Poppins: " << p_wt << " pounds.\n";
+
+    cout << "Convert to int => ";
+    cout << "Poppins: " << int (poppins) << " pounds.\n";
+
+    return 0;
+}
+
+/*
+Convert to double => Poppins: 128.8 pounds.
+Convert to int => Poppins: 129 pounds.
+*/
