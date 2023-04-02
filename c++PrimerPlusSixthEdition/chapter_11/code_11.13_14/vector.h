e00761908dec4a6877864e62402a078e1640c0dc
FightingBoom <1446227671@qq.com>
2023-04-02 16:56:09 +0800

    增加注释

--- c++PrimerPlusSixthEdition/chapter_11/code_11.13_14/vector.h
+++ c++PrimerPlusSixthEdition/chapter_11/code_11.13_14/vector.h
@@ -22,10 +22,10 @@ namespace VECTOR
         void set_y();
     public:
         Vector();
-        Vector(double n1, double n2, Mode form = RECT);
+        Vector(double n1, double n2, Mode form = RECT); // 默认使用直角坐标
         ~Vector();
 
-        void reset(double n1, double n2, Mode form = RECT);
+        void reset(double n1, double n2, Mode form = RECT); // 默认使用直角坐标
 
         double xval() const {return x;}
         double yval() const {return y;}
