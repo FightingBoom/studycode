4e89aa2a80d47823f2bff1010802d09f36d3f052
FightingBoom <1446227671@qq.com>
2023-08-18 08:13:43 +0800

    新增BrassPlus派生类构造函数实现

--- c++PrimerPlusSixthEdition/chapter_13/code_13.07_08/brass.cpp
+++ c++PrimerPlusSixthEdition/chapter_13/code_13.07_08/brass.cpp
@@ -60,4 +60,21 @@ void Brass::ViewAcct() const
 }
 
 
+/*************************************************/
+
+BrassPlus::BrassPlus(const string & s, long an, double bal,
+                    double ml, double r) : Brass(s, an, bal)
+{
+    maxLoan = ml;
+    owesBank = 0.0;
+    rate = r;
+}
+
+BrassPlus::BrassPlus(const Brass & ba, double ml, double r)
+    : Brass(ba)
+{
+    maxLoan = ml;
+    owesBank = 0.0;
+    rate = r;
+}
 
