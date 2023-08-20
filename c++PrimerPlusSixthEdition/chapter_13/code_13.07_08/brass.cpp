4c2f0a175b662bb12913416a6c42cc11d51ab232
FightingBoom <1446227671@qq.com>
2023-08-20 17:39:42 +0800

    新增BrassPlus派生类相关函数实现

--- c++PrimerPlusSixthEdition/chapter_13/code_13.07_08/brass.cpp
+++ c++PrimerPlusSixthEdition/chapter_13/code_13.07_08/brass.cpp
@@ -78,3 +78,51 @@ BrassPlus::BrassPlus(const Brass & ba, double ml, double r)
     rate = r;
 }
 
+void BrassPlus::ViewAcct() const
+{
+    format initialState = setFormat();
+    precis prec = cout.precision(2);
+
+    Brass::ViewAcct();
+    cout << "Maximum loan: $" << maxLoan << endl;
+    cout << "Owed to bank: $" << owesBank << endl;
+    cout.precision(3);
+    cout << "Loan Rate: " << 100 * rate << "%\n";
+    restore(initialState, prec);
+}
+
+void BrassPlus::Withdraw(double amt)
+{
+    format initialState = setFormat();
+    precis prec = cout.precision(2);
+
+    double bal = Balance();
+    if (amt <= bal)
+        Brass:Withdraw(amt);
+    else if (amt <= bal + maxLoan - owesBank)
+    {
+        double advance = amt - bal;
+        owesBank += advance * (1.0 + rate);
+        cout << "Bank advance: $" << advance << endl;
+        cout << "Finance charge: $" << advance * rate << endl;
+        Deposit(advance);
+        Brass::Withdraw(amt);
+    }
+    else
+        cout << "Credit limit exceeded. Transaction cancelled.\n";
+    
+    restore(initialState, prec);
+}
+
+format setFormat()
+{
+    return cout.setf(std::ios_base::fixed, std::ios_base::floatfield);
+}
+
+void restore(format f, precis p)
+{
+    cout.setf(f, std::ios_base::floatfield);
+    cout.precision(p);
+}
+
+
