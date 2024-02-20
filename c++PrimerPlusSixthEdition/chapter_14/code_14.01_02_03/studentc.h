fffd02b59acaa946ac90818a15045b92efd7bdff
FightingBoom <1446227671@qq.com>
2024-02-20 22:58:13 +0800

    新增14.1小节练习头文件，学习使用包含对象成员的类

---
+++ c++PrimerPlusSixthEdition/chapter_14/code_14.01_02_03/studentc.h
@@ -0,0 +1,51 @@
+#ifndef STUDENTC_H_
+#define STUDENTC_H_
+
+#include <iostream>
+#include <string>
+#include <valarray>
+
+class Student
+{
+private:
+    typedef std::valarray<double> ArrayDb;
+    std::string name;
+    ArrayDb scores;
+    std::ostream & arr_out(std::ostream & os) const;
+
+public:
+    Student()
+        : name("Null Student"), scores(){}
+
+    // explicit禁止隐式调用构造函数，但允许显式调用
+    explicit Student(const std::string & s)
+        : name(s), scores() {}
+
+    explicit Student(int n)
+        : name("Nully"), scores(n) {}
+
+    Student(const std::string & s, int n)
+        : name(s), scores(n) {}
+
+    Student(const std::string & s, const ArrayDb & a)
+        : name(s), scores(a) {}
+
+    Student(const char * str, const double * pd, int n)
+        : name(str), scores(pd, n) {}
+
+    ~Student() {}
+
+    double Average() const;
+    const std::string & Name() const;
+    double & operator[](int i);
+    double operator[](int i) const;
+
+    // 友元函数 输入字符
+    friend std::istream & operator>>(std::istream & is, Student & stu);
+    // 输入一行
+    friend std::istream & getling(std::istream & is, Student & stu);
+    // 输出
+    friend std::ostream & operator<<(std::ostream & os, const Student & stu);
+};
+
+#endif
