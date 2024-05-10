9f57aefafbdb852539a20296450cc259bf7cb16b
FightingBoom <1446227671@qq.com>
2024-05-10 23:31:40 +0800

    add 16.06 code.

---
+++ c++PrimerPlusSixthEdition/chapter_16/code_16.06/fowl.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+#include <memory>
+
+int main()
+{
+    using namespace std;
+
+    auto_ptr<string> films[5] =
+    {
+        auto_ptr<string> (new string("Fowl Balls")),
+        auto_ptr<string> (new string("Duck walks")),
+        auto_ptr<string> (new string("Chicken Runs")),
+        auto_ptr<string> (new string("Turkey Errors")),
+        auto_ptr<string> (new string("Goose Eggs"))
+    };
+
+    auto_ptr<string> pwin;
+    pwin = films[2];
+
+    cout << "The nominees for best avian "
+        << "baseball film are\n";
+
+    for (int i = 0; i < 5; i++)
+    {
+        cout << *films[i] << endl;
+    }
+
+    cout << "The winner is " << *pwin << "!\n";
+    cin.get();
+
+    return 0;
+}
+
+/*
+zhaochen@zhaochendeAir code_16.06 % clang++ fowl.cpp
+zhaochen@zhaochendeAir code_16.06 % ./a.out 
+The nominees for best avian baseball film are
+Fowl Balls
+Duck walks
+zsh: segmentation fault  ./a.out
+*/
