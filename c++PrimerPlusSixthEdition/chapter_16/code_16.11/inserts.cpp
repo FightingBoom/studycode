36c613349c5ca3875fc4e60665ea86d9ff100d3c
FightingBoom <1446227671@qq.com>
2024-05-26 20:26:42 +0800

    add 16.11 code.

---
+++ c++PrimerPlusSixthEdition/chapter_16/code_16.11/inserts.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include <iterator>
+#include <vector>
+#include <algorithm>
+
+void output(const std::string & s)
+{
+    std::cout << s << " ";
+}
+
+int main()
+{
+    using namespace std;
+
+    string s1[4] = {"fine", "fish", "fashion", "fate"};
+    string s2[2] = {"busy", "bats"};
+    string s3[2] = {"silly", "singers"};
+
+    vector<string> words(4);
+    copy(s1, s1 + 4, words.begin());
+    for_each(words.begin(), words.end(), output);
+    cout << endl;
+
+    // 在words末尾插入元素
+    copy(s2, s2 + 2,
+        back_insert_iterator< vector<string> >(words));
+    for_each(words.begin(), words.end(), output);
+    cout << endl;
+
+    // 自定义位置插入元素
+    copy(s3, s3 + 2, insert_iterator< vector<string> >
+        (words, words.begin()));
+    for_each(words.begin(), words.end(), output);
+    cout << endl;
+
+    return 0;
+}
+
+/*
+zhaochen@zhaochendeAir code_16.11 % ./a.out 
+fine fish fashion fate 
+fine fish fashion fate busy bats 
+silly singers fine fish fashion fate busy bats
+*/
