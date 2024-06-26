67f80d8383ddf0ee0d48301ae2f4900719506dba
FightingBoom <1446227671@qq.com>
2024-06-26 22:22:01 +0800

    add 17.04 code.

---
+++ c++PrimerPlusSixthEdition/chapter_17/code_17.04/width.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+int main()
+{
+    using std::cout;
+    int w = cout.width(30);
+    cout << "default field width = " << w << ":\n";
+
+    cout.width(5);
+    cout << "N" << ':';
+    cout.width(8);
+    cout << "N * N" << ":\n";
+
+    for (long i = 1; i <= 100; i *= 10)
+    {
+        cout.width(5);
+        cout << i << ':';
+        cout.width(8);
+        cout << i * i << ":\n";
+    }
+
+    return 0;
+}
+
+/*
+zhaochen@zhaochendeMacBook-Air code_17.04 % clang++ width.cpp 
+zhaochen@zhaochendeMacBook-Air code_17.04 % ./a.out 
+        default field width = 0:
+    N:   N * N:
+    1:       1:
+   10:     100:
+  100:   10000:
+*/
