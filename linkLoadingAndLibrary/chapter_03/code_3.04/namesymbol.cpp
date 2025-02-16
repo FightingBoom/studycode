d1bae3025f6f0103c58ac0e23ce69e2b05199d68
FightingBoom <1446227671@qq.com>
2025-02-16 23:04:41 +0800

    add linkLoading 3.04 study note.

---
+++ linkLoadingAndLibrary/chapter_03/code_3.04/namesymbol.cpp
@@ -0,0 +1,21 @@
+int func(int);
+float func(float);
+
+class C
+{
+    int func(int);
+    class C2
+    {
+        int func(int);
+    };
+};
+
+namespace N
+{
+    int func(int);
+    class C
+    {
+        int func(int);
+    };
+}
+
